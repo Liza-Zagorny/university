90a824b84b7ee05a8171ccd1ba7434de9712b0ea
Liza Neiman <5gb.place.for.love@gmail.com>
2023-08-27 14:12:04 +0300

    code execution

--- src/main/java/il/lizaneiman/universitytask/_main/_Main.java
+++ src/main/java/il/lizaneiman/universitytask/_main/_Main.java
@@ -1,7 +1,21 @@
 package il.lizaneiman.universitytask._main;
 
+import il.lizaneiman.universitytask.entity.Lecturer;
+import il.lizaneiman.universitytask.entity.Student;
+import il.lizaneiman.universitytask.entity.University;
+import il.lizaneiman.universitytask.util.LecturerService;
+import il.lizaneiman.universitytask.util.StudentService;
+
 public class _Main {
     public static void main(String[] args) {
 
+        University university1 = new University();
+        Lecturer teacher1 = new Lecturer("Иван", "Иванов", "123", "английский", "full-time", university1);
+        Student student1 = new Student(university1, teacher1, 1, "Java", "Aлиса", "Петрова", "456");
+        StudentService studentService = new StudentService();
+        LecturerService lecturerService = new LecturerService();
+
+        studentService.executeStudentActions(student1);
+        lecturerService.executeBasicActions(teacher1);
     }
 }
